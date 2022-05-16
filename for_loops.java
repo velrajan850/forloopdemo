6a0d1aa1e62bab37e62f33fe710318ae9a287a6e
velrajan850 <105338994+velrajan850@users.noreply.github.com>
2022-05-16 17:52:01 +0530

    Add files via upload

---
+++ for_loops.java
@@ -0,0 +1,15 @@
+package demoo;
+
+public class for_loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		for(i=1;i<=5;i++);
+		{
+			System.out.print("hello ");
+		}
+
+	}
+
+}
---
+++ while_loop.java
@@ -0,0 +1,16 @@
+package demoo;
+
+public class while_loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i =0;
+		while (i<=6);
+		{
+			System.out.print("helo world");
+			i++;
+		}
+
+	}
+
+}
